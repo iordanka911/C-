e6c48c7e1a5a9c64fe88230a48c2bb3332276da8
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2022-08-02 23:28:00 +0300

    Add files via upload

---
+++ c#oop/c#Exercises/01.Inheritance Lab/03. Hierarchical Inheritance/Dog.cs
@@ -0,0 +1,11 @@
+﻿using Farm;
+namespace Farm
+{
+    public class Dog : Animal
+    {
+        public void Bark()
+        {
+            System.Console.WriteLine("barking...");
+        }
+    }
+}
